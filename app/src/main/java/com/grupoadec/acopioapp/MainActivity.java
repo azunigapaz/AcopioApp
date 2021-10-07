c6aaff456a2215163ab9ede162f2bd1564870f67
Alejandro <azunigapaz@gmail.com>
2021-10-07 12:00:59 -0600

    PrimeraActualizacionAlRepositorio
    Primera actualizacion

---
+++ app/src/main/java/com/grupoadec/acopioapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.grupoadec.acopioapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

b8da730345b6d352b65ce25a5c6da28d5892ac51
josevigil27 <91038590+josevigil27@users.noreply.github.com>
2021-11-04 12:13:33 -0600

    Disenio Interfaz Acopio, Productores, Productos, Consultaz y Logo Principal

---
+++ app/src/main/java/com/grupoadec/acopioapp/ActivityListViewProductosSelectParaAcopio.java
@@ -0,0 +1,14 @@
+package com.grupoadec.acopioapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityListViewProductosSelectParaAcopio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view_productos_select_para_acopio);
+    }
+}

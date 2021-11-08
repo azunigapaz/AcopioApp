3706f48ecfbb1191bcf5c9acfcfc105c93452596
josevigil27 <91038590+josevigil27@users.noreply.github.com>
2021-11-07 21:19:11 -0600

    Disenio Activity Configuracion y Agregar Producto y Validacion de campos MaxLength y detalles para listview

---
+++ app/src/main/java/com/grupoadec/acopioapp/ActivityAgregarProductosParaAcopio.java
@@ -0,0 +1,14 @@
+package com.grupoadec.acopioapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityAgregarProductosParaAcopio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_productos_para_acopio);
+    }
+}

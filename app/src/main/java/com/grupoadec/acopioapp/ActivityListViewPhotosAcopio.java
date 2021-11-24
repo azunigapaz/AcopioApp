ad2e4eac58d1007c4a7d5ad141d487402b380b0a
josevigil27 <91038590+josevigil27@users.noreply.github.com>
2021-11-24 11:27:56 -0600

    CAI, Fecha Limite y Rango Autorizado en Configuracion. Disenio ActivityPhoto en el AcopioMain

---
+++ app/src/main/java/com/grupoadec/acopioapp/ActivityListViewPhotosAcopio.java
@@ -0,0 +1,14 @@
+package com.grupoadec.acopioapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityListViewPhotosAcopio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view_photos_acopio);
+    }
+}
